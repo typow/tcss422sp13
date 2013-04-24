dfa298ed8517bd950d9e74d342f03e5ba2915d1a
sethk2@uw.edu <sethk2@uw.edu@94fc05b0-ceb9-3df0-bd89-ab10b82c0e3e>
2013-04-24 00:59:44 +0000

    Still pretty shitty

--- trunk/spider/spider/src/PageRetriever.java
+++ trunk/spider/spider/src/PageRetriever.java
@@ -1,7 +1,10 @@
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayDeque;
 import java.util.concurrent.locks.Lock;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 
@@ -12,27 +15,41 @@ public class PageRetriever implements Runnable {
 	private Document myDoc;
 	private Lock pageToRetrieveLock;
 	private Lock pageBufferLock;
+	private ArrayDeque<URL> pageToRetrieve;
+	private ArrayDeque<Document> pageBuffer;
 
-	public PageRetriever(Lock retrieveLock, Lock bufferLock) {
+	public PageRetriever(Lock retrieveLock, Lock bufferLock,
+							ArrayDeque<URL> retrieveQueue, ArrayDeque<Document> bufferQueue) {
 		pageToRetrieveLock = retrieveLock;
 		pageBufferLock = bufferLock;
+		pageToRetrieve = retrieveQueue;
+		pageBuffer = bufferQueue;
+		
 		try { 
 			myURL = new URL("");
 		} catch (MalformedURLException e) {}
 	}
 	
-	public void setURL(URL u) {
-		myURL = u;
-	}
 	public URL getURL() {
 		return myURL;
 	}
 	
 	public void run() {
-		if (pageToRetrieveLock.tryLock()) {
-			
+		
+		do {
+			pageToRetrieveLock.lock();
+			myURL = pageToRetrieve.removeFirst();
 			pageToRetrieveLock.unlock();
-	    }
-	      
+		
+			try {
+				myDoc = Jsoup.connect(myURL.toString()).get();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+			pageBufferLock.lock();
+			pageBuffer.addLast(myDoc);
+			pageBufferLock.unlock();
+		} while (true);
 	}
 }
