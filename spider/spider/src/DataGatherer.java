c68c8e31ef50f90bd687aaa8225f65a30f9f6b8c
sethk2@uw.edu <sethk2@uw.edu@94fc05b0-ceb9-3df0-bd89-ab10b82c0e3e>
2013-05-02 00:37:22 +0000

    Got the testing section setup to print to file

--- spider/spider/src/DataGatherer.java
+++ spider/spider/src/DataGatherer.java
@@ -1,7 +1,11 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
+
 import org.jsoup.nodes.Document;
 
 
@@ -48,6 +52,35 @@ public class DataGatherer extends Thread {
 	public void run() {
 		String texts;
 		int retrieveCount = 0;
+		
+		//FileOutputStream out; 
+		//PrintStream ps = null; 
+		PrintWriter ps = null;
+		
+		/*          For creating a file and printing to it
+		try {
+			 // Create a new file output stream
+			  out = new FileOutputStream("spiderRunST.txt");
+
+			  // Connect print stream to the output stream
+			  ps = new PrintStream(out);
+		}
+		  catch (Exception e){
+		  System.err.println ("Error in writing to file");
+		 }
+		 */
+		
+		try  
+		{
+			File file = new File("spiderRunST.txt");
+		    FileWriter fstream = new FileWriter(file, true); //true tells to append data.
+		    ps = new PrintWriter(fstream);
+		}
+		catch (Exception e)
+		{
+		    System.err.println("Error: " + e.getMessage());
+		}
+		  
 		do {
 			synchronized (myGatherer) {
 				retrieveDoc();
@@ -55,7 +88,7 @@ public class DataGatherer extends Thread {
 			}
 	
 			String str;
-
+			
 			try {
 				texts = myDoc.body().text();
 				Scanner stringscan = new Scanner(texts);
@@ -88,6 +121,8 @@ public class DataGatherer extends Thread {
 					Average parse time per page .001msec
 					Total running time:       0.96 sec
 									 */
+			
+			
 				System.out.printf("\n\n\n");
 				System.out.println("Parsed: " + myBigStruct.getUrlName());
 				System.out.println("Pages Retrieved: " + retrieveCount);
@@ -103,6 +138,25 @@ public class DataGatherer extends Thread {
 				System.out.printf("Total running time: %.4f seconds\n", (totalTime * (Math.pow(10, -9))));
 				stringscan.close();
 				
+				
+				// Printing the data to a file
+				/*
+				ps.printf("\n\n\n");
+				ps.println("Parsed: " + myBigStruct.getUrlName());
+				ps.println("Pages Retrieved: " + retrieveCount);
+				ps.println("Average Words Per Page: " + (totalWordCount / retrieveCount));
+				ps.println("Average URL's per page: " + (totalUrlCount / retrieveCount));
+				ps.printf("Keyword \tAvg. hits per page \tTotal hits\n");
+				
+				for (Map.Entry<String, Integer> word : myMap.entrySet()) {
+					ps.printf("  %-20s %-20d %-20d\n", word.getKey(), word.getValue() / retrieveCount, word.getValue());
+				}
+				ps.println("Page limit: " + count);
+				ps.printf("Average parse time per page: %.4f seconds\n", (totalParseTime / retrieveCount) * (Math.pow(10, -9)));
+				ps.printf("Total running time: %.4f seconds\n", (totalTime * (Math.pow(10, -9))));
+				*/			
+				
+				
 			} catch (NullPointerException e) {
 				// Throw away docs with empty bodies
 			}
@@ -110,7 +164,14 @@ public class DataGatherer extends Thread {
 				updateCount();
 			}
 		} while (continueRunning);
-		//System.out.println("Total time: " + totalTime * (Math.pow(10, -9)) + "seconds");
+		ps.println();
+		ps.println();
+		ps.println("------------------------------------------------------------------------");
+		ps.println();
+		ps.printf("Average parse time per page: %.4f seconds\n", (totalParseTime / retrieveCount) * (Math.pow(10, -9)));
+		ps.println();
+		ps.println("Total time: " + totalTime * (Math.pow(10, -9)) + " seconds");
+		ps.close();
 	
 	}
 
